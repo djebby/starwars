8e15b6736369b4d94c0397f6ab06d94027f818ab
djebby <djebby.firas@gmail.com>
2021-12-03 18:23:29 +0100

    adding the link of the main branch to the footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -10,7 +10,7 @@ export default function Footer() {
         <div className="footer">
             <p> I create this React js application in order to be more familiar with this great library and deal more with axios,router,react bootstrap, responsive web design, github pages...
                 <br />
-                this <a href="#" style={{color:"#61dafb",textDecoration:"none"}}> link </a> guid you to the github repository of the development version.
+                this <a href="https://github.com/djebby/starwars/tree/main" style={{color:"#61dafb",textDecoration:"none"}}> link </a> guid you to the github repository of the development version.
             </p>
             <div>
             <a style={{textDecoration: "none"}} href="https://github.com/djebby" target="_blank"> 
