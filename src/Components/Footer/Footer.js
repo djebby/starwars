13a1e60b18c10c90c7136faad6aaf5635393e6c0
djebby <djebby.firas@gmail.com>
2021-12-04 12:26:40 +0100

    replace BrowserRouter with HashRouter

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,21 +1,17 @@
-
-
 import "./footer.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
 
 export default function Footer() {
     return (
         <div className="footer">
             <p> I create this React js application in order to be more familiar with this great library and deal more with axios,router,react bootstrap, responsive web design, github pages...
                 <br />
-                this <a href="https://github.com/djebby/starwars/tree/main" style={{color:"#61dafb",textDecoration:"none"}}> link </a> guid you to the github repository of the development version.
+                this <a href="https://github.com/djebby/starwars/tree/main" target="_blank" style={{color:"#61dafb",textDecoration:"none"}}> link </a> guid you to the github repository of the development version.
             </p>
             <div>
             <a style={{textDecoration: "none"}} href="https://github.com/djebby" target="_blank"> 
             <i class="bi bi-github"></i></a>
-            <a style={{textDecoration: "none"}} href="https://www.linkedin.com/in/djebby-firas/"> <i class="bi bi-linkedin"></i> </a>
+            <a style={{textDecoration: "none"}} href="https://www.linkedin.com/in/djebby-firas/" target="_blank"> <i class="bi bi-linkedin"></i> </a>
             </div>
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Components/App/App.js';
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter,HashRouter} from "react-router-dom";
 
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>,document.getElementById('root'));
+ReactDOM.render(<HashRouter><App /></HashRouter>,document.getElementById('root'));
